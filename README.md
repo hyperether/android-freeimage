5da37fd2aef4e4790b8a5c0c95f6c30362e5b19c
Slobodan <slobodan.prijic@gmail.com>
2020-04-02 12:09:14 +0200

    Initial code release

---
+++ README.md
@@ -0,0 +1,10 @@
+# FreeImage-Android
+FreeImage compiled for Android
+
+update to 3.18
+
+LibWebp fajlovi izbacuju gresku da nije dobra putanja
+Pokusaj 1: $(LOCAL_PATH)/Source/LibWebP \
+$(LOCAL_PATH)/Source/LibWebP/src \
+Dodao u android mk (jamcar u fajlovima koristi putanje sa tackama ./, ../)
+
---
+++ jni/Application.mk
@@ -0,0 +1,8 @@
+ADDITIONAL_FLAGS := -O3 -fopenmp -DANDROID
+APP_CFLAGS += $(ADDITIONAL_FLAGS) -DPNG_ARM_NEON_OPT=0
+APP_CONLYFLAGS += -std=c11
+APP_CPPFLAGS += $(ADDITIONAL_FLAGS) -std=c++11 -fexceptions -frtti -DPNG_ARM_NEON_OPT=0
+APP_PLATFORM := android-28
+APP_STL   := c++_static
+APP_ABI   := armeabi-v7a arm64-v8a
+APP_OPTIM := debug
